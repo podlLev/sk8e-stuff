c8c97a3023fcf23093a41a2bc1e42179b9caae2e
podlLev <pidlisnyilev@gmail.com>
2022-08-25 22:38:35 +0300

    Breadcrumbs completion

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
+from django.urls import reverse
+
 from apps.blog.models import BlogCategory, Article, Tag
+from config.settings import PAGE_NAMES
 
 
 def blog_category_list(request):
     blog_categories = BlogCategory.objects.all()
-    return render(request, 'blog/category/list.html', {'categories': blog_categories})
+    breadcrumbs = {'current': PAGE_NAMES['blog']}
+    return render(request, 'blog/category/list.html', {'categories': blog_categories, 'breadcrumbs': breadcrumbs})
 
 
 def article_list(request, category_id):
     articles = Article.objects.filter(category=category_id)
     category = BlogCategory.objects.get(id=category_id)
-    return render(request, 'blog/article/list.html', {'category': category, 'articles': articles})
+    breadcrumbs = {
+        reverse('blog_category_list'): PAGE_NAMES['blog'],
+        'current': category.name
+    }
+    return render(request, 'blog/article/list.html',
+                  {'category': category, 'articles': articles, 'breadcrumbs': breadcrumbs})
 
 
 def article_view(request, category_id, article_id):
     article = Article.objects.get(id=article_id)
     category = BlogCategory.objects.get(id=category_id)
-    return render(request, 'blog/article/view.html', {'article': article, 'category': category})
+    breadcrumbs = {
+        reverse('blog_category_list'): PAGE_NAMES['blog'],
+        reverse('article_list', args=[category.id]): category.name,
+        'current': article.title
+    }
+    return render(request, 'blog/article/view.html',
+                  {'article': article, 'category': category, 'breadcrumbs': breadcrumbs})
 
 
 def tag_article_list(request, tag_id):
     tag = Tag.objects.get(id=tag_id)
     articles = Article.objects.filter(tags__in=[tag_id])
-    return render(request, 'blog/tag/list.html', {'articles': articles, 'tag': tag})
+    breadcrumbs = {
+        reverse('blog_category_list'): PAGE_NAMES['blog'],
+        'current': f"Поиск по #{tag.name}"
+    }
+    return render(request, 'blog/tag/list.html', {'articles': articles, 'tag': tag, 'breadcrumbs': breadcrumbs})
