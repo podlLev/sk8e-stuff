1c6063cac688a99faccddd06b87c9b63de35938a
podlLev <pidlisnyilev@gmail.com>
2022-09-08 18:15:04 +0300

    Order create part 1

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -4,4 +4,5 @@ from apps.order import views
 urlpatterns = [
     path('add/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_product_list, name='cart_product_list'),
+    path('create/', views.create_order, name='create_order'),
 ]
