2c1a629038abd2b103b2b1fcee96515127546d49
podlLev <pidlisnyilev@gmail.com>
2022-09-29 18:24:56 +0300

    DRF blog CRUD part 2

--- apps/api/blog/views.py
+++ apps/api/blog/views.py
@@ -1,11 +1,36 @@
-from rest_framework import generics, permissions, viewsets
-from apps.blog.models import Article
-from apps.api.blog.serializers import ArticleSerializer
+from rest_framework import generics, permissions, viewsets, status
+from rest_framework.response import Response
+
+from apps.blog.models import Article, Tag
+from apps.api.blog.serializers import ArticleReadSerializer, ArticleWriteSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAdminUser]
-    serializer_class = ArticleSerializer
+    serializer_class = ArticleReadSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update']:
+            return ArticleWriteSerializer
+        return self.serializer_class
+
+    @staticmethod
+    def check_tags(tags):
+        tags_list = []
+        for item in tags:
+            tag = Tag.objects.filter(name=item).first()
+            if not tag:
+                tag = Tag.objects.create(name=item)
+            tags_list.append(tag)
+        return tags_list
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        tags = serializer.validated_data.get('tags')
+        article = serializer.save(user=self.request.user, tags=self.check_tags(tags))
+        read_serializer = self.serializer_class(article, context={'request': request})
+        return Response(read_serializer.data, status=status.HTTP_201_CREATED)
 
     def get_queryset(self):
         queryset = Article.objects.all()
@@ -17,6 +42,3 @@ class ArticleViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(category=self.request.query_params['category'])
 
         return queryset
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
